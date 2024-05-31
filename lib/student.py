5ab0a2b91d475e87354a12e8b78e02a41a117519
Lenny Wandeto <lenny.wandeto@student.moringaschool.com>
2024-05-31 10:34:11 +0300

    completion

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
+    knowledge = []
+    def __init__(self, first_name, last_name) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        super().__init__(first_name, last_name)
     
-    def learn(self):
+    def learn(self, string):
+        Student.knowledge.append(string)
         pass
--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,15 @@ from user import User
 import random
 
 class Teacher(User):
-
+    knowledge = ["wp"]
+    def __init__(self, first_name, last_name) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        super().__init__(first_name, last_name)
     def teach(self):
+        if len(Teacher.knowledge) == 0:
+            return None
+        else:
+            for knowl in Teacher.knowledge:
+                return knowl
         pass
